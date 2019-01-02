08a5b586265674a6ce60b7fbcdb2f0163ec4f560
Tzu-Mao Li <tzumao@mit.edu>
2019-01-02 18:37:54 -0500

    1d conv benchmark

--- apps/autoscheduler/benchmark_gradient.cpp
+++ apps/autoscheduler/benchmark_gradient.cpp
@@ -4,14 +4,23 @@
 
 #include <dlfcn.h>
 #include <iostream>
+#include <string>
 
 using namespace Halide;
 using namespace Halide::Tools;
 
 int main(int argc, char **argv) {
-    if (!dlopen("libauto_schedule.so", RTLD_LAZY)) {
-        std::cerr << "Failed to load autoscheduler: " << dlerror() << "\n";
-        return 1;
+    std::string use_master_autoscheduler =
+        Halide::Internal::get_env_variable("HL_USE_MASTER_AUTOSCHEDULER");
+    if (use_master_autoscheduler.empty()) { 
+        std::cout << "Use new autoscheduler" << std::endl;
+        if (!dlopen("libauto_schedule.so", RTLD_LAZY)) {
+            std::cerr << "Failed to load autoscheduler: " << dlerror() << "\n";
+            return 1;
+        }
+    } else {
+        std::cout << "Use master autoscheduler" << std::endl;
+        Pipeline::set_custom_auto_scheduler(nullptr);
     }
 
     MachineParams params(32, 16000000, 40);
@@ -42,6 +51,34 @@ int main(int argc, char **argv) {
         p.compile_jit(target);
         double best_time = benchmark(timing_iterations, 10, [&]() {
             p.realize(d_f_buffer);
+        });
+        std::cout << "best time:" << best_time << std::endl;
+    }
+
+    if (1) {
+        std::cout << "1D conv" << std::endl;
+        int n = 1000000;
+        Func f("f"), g("g"), h("h"), k("k");
+        RDom r(0, 5);
+        f(x) = cast<float>(x);
+        k(x) = cast<float>(x);
+        g(x) += f(x - r) * k(r);
+        RDom r_f(0, n);
+        h() += g(r_f);
+
+        auto d = propagate_adjoints(h);
+        Func d_f = d(f);
+        Func d_k = d(k);
+
+        d_f.estimate(x, 0, n);
+        d_k.estimate(x, 0, 5);
+
+        Pipeline p = Pipeline({d_f, d_k});
+        p.auto_schedule(target, params);
+        Buffer<float> d_f_buffer(n), d_k_buffer(5);
+        p.compile_jit(target);
+        double best_time = benchmark(timing_iterations, 10, [&]() {
+            p.realize({d_f_buffer, d_k_buffer});
         });
         std::cout << "best time:" << best_time << std::endl;
     }
