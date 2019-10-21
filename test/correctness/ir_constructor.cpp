cffbe1c25b05862edf1b05803e47b78950300ce3
Tzu-Mao Li <tzumao@mit.edu>
2019-10-21 14:47:41 -0700

    refactor

--- test/correctness/ir_constructor.cpp
+++ test/correctness/ir_constructor.cpp
@@ -1,44 +1,60 @@
 #include "Halide.h"
+#include <vector>
 
 using namespace Halide;
 using namespace Halide::Internal;
+using std::vector;
 
-int main(int argc, char *argv[]) {
-    // Func f("f");
-    // f() = 0;
-    // Target target = get_jit_target_from_environment();
-    // target.set_feature(Target::NoBoundsQuery, true);
-    // target.set_feature(Target::NoAsserts, true);
-    // f.compile_jit(target);
-
-    Buffer<int> out = Buffer<int>::make_scalar("f");
-
-    Parameter f(Int(32), true /*is_buffer*/, 0, out.name());
-    Stmt s;
-    s = Store::make(f.name(), 100, 0, f, const_true(), ModulusRemainder());
-    s = ProducerConsumer::make_produce(f.name(), s);
-
-    std::cerr << s << std::endl;
-
+JITModule compile(const vector<Buffer<>> &inputs,
+                  const vector<Buffer<>> &outputs,
+                  Stmt s) {
     Target target = get_jit_target_from_environment();
     target.set_feature(Target::NoBoundsQuery, true);
     target.set_feature(Target::NoAsserts, true);
     target.set_feature(Target::JIT, true);
 
+    vector<Argument> arguments;
+    for (auto b : inputs) {
+        arguments.push_back(Argument(b));
+    }
+    for (auto b : outputs) {
+        arguments.push_back(Argument(b));
+    }
     const std::string &name = "foo";
     Module module = lower_from_stmt(s,
                                     name,
                                     target,
-                                    {Argument(out)},
+                                    arguments,
                                     LinkageType::ExternalPlusMetadata);
     auto func = module.get_function_by_name(name);
-    JITModule jit_module(module, func);
+    return JITModule(module, func);
+}
 
-    const void *raw_args[64] = {nullptr};
-    const halide_buffer_t *buf = out.raw_buffer();
-    raw_args[0] = (const void*)buf;
-    int exit = jit_module.argv_function()(raw_args);
+void run(JITModule m,
+         const vector<Buffer<>> &inputs,
+         const vector<Buffer<>> &outputs) {
+    std::vector<const void*> raw_args;
+    raw_args.reserve(inputs.size() + outputs.size());
+    for (auto b : inputs) {
+        raw_args.push_back((const void*)b.raw_buffer());
+    }
+    for (auto b : outputs) {
+        raw_args.push_back((const void*)b.raw_buffer());
+    }
+    int exit = m.argv_function()(raw_args.data());
     _halide_user_assert(exit == 0);
-    std::cerr << "out:" << out() << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    Buffer<int> out = Buffer<int>::make_scalar("f");
+    Parameter f(Int(32), true /*is_buffer*/, 0, out.name());
+
+    Stmt s;
+    s = Store::make(f.name(), 100, 0, f, const_true(), ModulusRemainder());
+    s = ProducerConsumer::make_produce(f.name(), s);
+
+    JITModule m = compile({}, {out}, s);
+    run(m, {}, {out});
+    std::cout << "out:" << out() << std::endl;
     return 0;
 }
