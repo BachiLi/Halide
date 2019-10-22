3adfd87c2d967d48fd30f620cdee7d07e418e861
Tzu-Mao Li <tzumao@mit.edu>
2019-10-21 19:47:19 -0700

    ForAll wrapper

--- apps/frontends/frontends.cpp
+++ apps/frontends/frontends.cpp
@@ -1,12 +1,16 @@
 #include "Halide.h"
+#include "Errors.h"
 #include "Lower.h"
 #include <vector>
+#include <functional>
 
 using namespace Halide;
 using namespace Halide::Internal;
 using std::vector;
 using std::map;
 using std::string;
+using std::pair;
+using std::function;
 
 Parameter parameter(const Buffer<> &buffer) {
     return Parameter(buffer.type(),
@@ -61,6 +65,28 @@ void run(JITModule m,
     int exit = m.argv_function()(raw_args.data());
     _halide_user_assert(exit == 0);
 }
+
+/////////////////////////////////////////////////////////
+// Helpers for constructing statements
+Stmt ForAll(const vector<string> &names,
+            const vector<pair<Expr, Expr>> &ranges,
+            function<Stmt()> f,
+            ForType for_type = ForType::Serial,
+            DeviceAPI device_api = DeviceAPI::Host) {
+    user_assert(names.size() == ranges.size());
+    Stmt s = f();
+    // Build loop nest
+    for (int i = 0; i < (int)names.size(); i++) {
+        s = For::make(names[i],
+                      ranges[i].first,
+                      ranges[i].second,
+                      for_type,
+                      device_api,
+                      s);
+    }
+    return s;
+}
+/////////////////////////////////////////////////////////
 
 /// Store 100 to a scalar function
 void store_to_scalar() {
@@ -120,11 +146,12 @@ void provide_loop() {
     Parameter f = parameter(out);
 
     Stmt s;
-    Expr x = Variable::make(Int(32), "x");
-    s = Provide::make(f.name(), {x}, {x});
     Expr min = Variable::make(Int(32), out.name() + ".min.0");
     Expr extent = Variable::make(Int(32), out.name() + ".extent.0");
-    s = For::make("x", min, extent, ForType::Serial, DeviceAPI::Host, s);
+    s = ForAll({"x"}, {{min, extent}}, [&]() {
+        Expr x = Variable::make(Int(32), "x");
+        return Provide::make(f.name(), {x}, {x});
+    });
     s = ProducerConsumer::make_produce(f.name(), s);
 
     JITModule m = compile({}, {out}, {f}, s);
@@ -140,15 +167,15 @@ void provide_loop_multidim() {
     Parameter f = parameter(out);
 
     Stmt s;
-    Expr x = Variable::make(Int(32), "x");
-    Expr y = Variable::make(Int(32), "y");
-    s = Provide::make(f.name(), {x + y}, {x, y});
     Expr x_min = Variable::make(Int(32), out.name() + ".min.0");
     Expr x_extent = Variable::make(Int(32), out.name() + ".extent.0");
-    s = For::make("x", x_min, x_extent, ForType::Serial, DeviceAPI::Host, s);
     Expr y_min = Variable::make(Int(32), out.name() + ".min.1");
-    Expr y_extent = Variable::make(Int(32), out.name() + ".extent.1");
-    s = For::make("y", y_min, y_extent, ForType::Serial, DeviceAPI::Host, s);
+    Expr y_extent = Variable::make(Int(32), out.name() + ".extent.1");    
+    s = ForAll({"x", "y"}, {{x_min, x_extent}, {y_min, y_extent}}, [&]() {
+        Expr x = Variable::make(Int(32), "x");
+        Expr y = Variable::make(Int(32), "y");
+        return Provide::make(f.name(), {x + y}, {x, y});
+    });
     s = ProducerConsumer::make_produce(f.name(), s);
 
     JITModule m = compile({}, {out}, {f}, s);
@@ -168,16 +195,16 @@ void call_provide_loop_multidim() {
     Parameter g = parameter(out);
 
     Stmt s;
-    Expr x = Variable::make(Int(32), "x");
-    Expr y = Variable::make(Int(32), "y");
-    Expr e = 2 * Call::make(in, {x, y});
-    s = Provide::make(g.name(), {e}, {x, y});
     Expr x_min = Variable::make(Int(32), out.name() + ".min.0");
     Expr x_extent = Variable::make(Int(32), out.name() + ".extent.0");
-    s = For::make("x", x_min, x_extent, ForType::Serial, DeviceAPI::Host, s);
     Expr y_min = Variable::make(Int(32), out.name() + ".min.1");
     Expr y_extent = Variable::make(Int(32), out.name() + ".extent.1");
-    s = For::make("y", y_min, y_extent, ForType::Serial, DeviceAPI::Host, s);
+    s = ForAll({"x", "y"}, {{x_min, x_extent}, {y_min, y_extent}}, [&]() {
+        Expr x = Variable::make(Int(32), "x");
+        Expr y = Variable::make(Int(32), "y");
+        Expr e = 2 * Call::make(in, {x, y});
+        return Provide::make(g.name(), {e}, {x, y});
+    });
     s = ProducerConsumer::make_produce(g.name(), s);
 
     for (int i = 0; i < 16; i++) {
@@ -209,25 +236,26 @@ void in_place_bubble_sort() {
     //     }
     // }
     Stmt s;
-    Expr i = Variable::make(Int(32), "i");
-    Expr j = Variable::make(Int(32), "j");
-    Expr prev = Call::make(in_out, {j - 1});
-    Expr curr = Call::make(in_out, {j});
-    // _t = f(j - 1)
-    s = Store::make("_t", prev, 0, Parameter(), const_true(), ModulusRemainder());
-    // f(j - 1) = f(j)
-    s = Block::make(s, Provide::make(f.name(), {curr}, {j - 1}));
-    // f(j) = _t
-    Expr _t = Load::make(Int(32), "_t", 0, Buffer<>(), Parameter(), const_true(), ModulusRemainder());
-    s = Block::make(s, Provide::make(f.name(), {_t}, {j}));
-    // Allocate(_t)
-    s = Allocate::make("_t", Int(32), MemoryType::Register, {}, const_true(), s);
-    // if (f(j - 1) > f(j))
-    s = IfThenElse::make(prev > curr, s);
     Expr f_min = Variable::make(Int(32), in_out.name() + ".min.0");
     Expr f_extent = Variable::make(Int(32), in_out.name() + ".extent.0");
-    s = For::make("j", f_min + 1, f_extent - 1, ForType::Serial, DeviceAPI::Host, s);
-    s = For::make("i", f_min, f_extent, ForType::Serial, DeviceAPI::Host, s);
+    s = ForAll({"j", "i"}, {{f_min + 1, f_extent - 1}, {f_min, f_extent}}, [&]() {
+        Expr i = Variable::make(Int(32), "i");
+        Expr j = Variable::make(Int(32), "j");
+        Expr prev = Call::make(in_out, {j - 1});
+        Expr curr = Call::make(in_out, {j});
+        // _t = f(j - 1)
+        Stmt s = Store::make("_t", prev, 0, Parameter(), const_true(), ModulusRemainder());
+        // f(j - 1) = f(j)
+        s = Block::make(s, Provide::make(f.name(), {curr}, {j - 1}));
+        // f(j) = _t
+        Expr _t = Load::make(Int(32), "_t", 0, Buffer<>(), Parameter(), const_true(), ModulusRemainder());
+        s = Block::make(s, Provide::make(f.name(), {_t}, {j}));
+        // Allocate(_t)
+        s = Allocate::make("_t", Int(32), MemoryType::Register, {}, const_true(), s);
+        // if (f(j - 1) > f(j))
+        s = IfThenElse::make(prev > curr, s);
+        return s;
+    });
     s = ProducerConsumer::make_produce(f.name(), s);
 
     for (int i = 0; i < 16; i++) {
