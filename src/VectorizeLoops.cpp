31e99982eeb779e2aa67ff132708578c34e5d6fe
Tzu-Mao Li <tzumao@mit.edu>
2020-04-11 18:56:14 -0400

    bring back predicated load/store

--- src/VectorizeLoops.cpp
+++ src/VectorizeLoops.cpp
@@ -271,8 +271,7 @@ class PredicateLoadStore : public IRMutator {
             // no less than 4
             // TODO: disabling for now due to trunk LLVM breakage.
             // See: https://github.com/halide/Halide/issues/3534
-            // return (bit_size == 32) && (lanes >= 4);
-            return false;
+            return (bit_size == 32) && (lanes >= 4);
         }
         // For other architecture, do not predicate vector load/store
         return false;
